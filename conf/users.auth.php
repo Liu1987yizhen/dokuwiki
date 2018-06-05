ed6ca104c1b72592f469b6cd04fef6bdcf7a1196
liuyizhen <liu1987yizhen@163.com>
2018-06-05 19:55:05 +0800

    wiki first commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+root:$1$UWEO9j1o$eYwUBKldx9nbtqPJhjWa11:root:liuyizhen_kcy@outlook.com:admin,user
